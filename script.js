02c357c14a1a701273b417f660354480bde324e9
kulfi28 <asabishek28@gmail.com>
2023-10-28 12:30:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var btn = document.getElementById("btn");
+var light = document.getElementById("light");
+var bulb = document.getElementById("flight");
+function togglebtn(){
+light.classList.toggle("on");
+btn.classList.toggle("active");
+bulb.classList.toggle("off")
+}
+
+
